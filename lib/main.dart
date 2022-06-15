5290a2ef908fe8483a215794bff83ad841cbd208
Vasajj <talapa.vasyl@gmail.com>
2022-06-15 13:14:27 +0300

    Privacy Policy

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,13 @@ import 'package:flutter/services.dart';
 import 'enter_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'philosophical_page.dart';
-
-
+import 'firebase_options.dart';
 
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
    await Firebase.initializeApp(
-       // options: DefaultFirebaseOptions.currentPlatform
+       options: DefaultFirebaseOptions.currentPlatform
    );
 
 
